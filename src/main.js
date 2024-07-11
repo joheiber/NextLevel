4489c690e13a61317a85edef103224eec713a795
joheiber almao <joheiberalmao1214@gmail.com>
2024-07-10 22:13:09 -0500

    hice el carrusel funcione solo falta maquetar el carrusel

--- src/main.js
+++ src/main.js
@@ -6,47 +6,58 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(`#mostrarBtn`).click(function () {
-        $('#menuDesplegable').toggle()
+  $(`#mostrarBtn`).click(function () {
+    $('#menuDesplegable').toggle()
+  })
+
+  $(`#desplegarSudMenu`).click(function () {
+    $('#subMenu').toggle()
+  })
+
+  fetch("https://api.rawg.io/api/games?key=ec99b7fa07b847acb46058d5b8200062")
+    .then((repuesta) => {
+      if (repuesta.ok) {
+        console.log()
+        return repuesta.json()
+      } else {
+        console.error(`error:${repuesta.statusText}`)
+      }
+    }).then((data) => {
+      console.log(data.results)
+      renderSwiper(data.results);
+    })
+    .catch((error) => {
+      console.error(`error:${error}`)
     })
 
-    $(`#desplegarSudMenu`).click(function () {
-        $('#subMenu').toggle()
-    })
+  const renderSwiper = (games) => {
+    const swiperWrapper = $('.swiper-wrapper');
 
-    $(document).ready(function(){
-        $('.carousel').slick({
-          dots: true,
-          infinite: true,
-          speed: 500,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
-        });
-      });
+    games.forEach(game => {
+      $("#swiper-wrapper").append(
+        ` <div class="swiper-slide"><img src="${game.background_image}" alt="${game.name}"></div>`
+      );
+    });
+
+    // Inicializar Swiper después de renderizar los slides
+    const swiper = new Swiper('.swiper', {
+      // configure Swiper to use modules
+      modules: [Navigation, Pagination],
+      // Optional parameters
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
 })
-
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-});
-
-
