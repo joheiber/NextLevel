09102afa4dca431d44400dc2ab6ff3595f49c7e7
joheiber almao <joheiberalmao1214@gmail.com>
2024-07-10 21:35:42 -0500

    termine de configuar a la aplicacion para usar swiper

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 $(document).ready(function(){
 
     $(`#mostrarBtn`).click(function () {
@@ -20,3 +28,25 @@ $(document).ready(function(){
         });
       });
 })
+
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+});
+
+
